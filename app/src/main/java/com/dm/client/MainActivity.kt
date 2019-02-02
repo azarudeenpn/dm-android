6ab1418107f61598726b0b2dd0aefc1bcf9c9815
Aslam Rasheed <aslampr07@outlook.com>
2019-02-02 13:48:46 +0530

    Added a button function

--- app/src/main/java/com/dm/client/MainActivity.kt
+++ app/src/main/java/com/dm/client/MainActivity.kt
@@ -2,11 +2,16 @@ package com.dm.client
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun buttonClick(view: View){
+        Toast.makeText(this, "Testing Button", Toast.LENGTH_LONG).show()
     }
 }
