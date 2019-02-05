1e05c2afb117229abd5cb4f9b1474bae8f8e52a7
Aslam Rasheed <aslampr07@outlook.com>
2019-02-05 11:17:53 +0530

    Enable location permission prompt

--- app/src/main/java/com/dm/client/MainActivity.kt
+++ app/src/main/java/com/dm/client/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.dm.client
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.View
+import android.widget.Toast
 import com.dm.client.victimregistration.VictimRegisterActivity
 import com.dm.client.volunteerregistration.VolunteerRegisterActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val PERMISSIONREQUEST = 1251
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Checking weather location is enabled.
+        if(isLocationEnabled())
+            Toast.makeText(this, "Location is Enabled", Toast.LENGTH_LONG).show()
+        else
+            openPermissionPrompt()
     }
-    fun buttonClick(view: View){
-        when(view.id){
+
+
+    private fun isLocationEnabled(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+                return true
+            }
+            return false
+        }
+        return false
+    }
+
+    private fun openPermissionPrompt() {
+        //When never again is given.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+        }
+        else{
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSIONREQUEST)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSIONREQUEST -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //Permission is granted.
+                }
+                else{
+                    Toast.makeText(this, "App cannot function without permission", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    fun buttonClick(view: View) {
+        when (view.id) {
             R.id.Main_VolunteerButton -> {
                 val i = Intent(this, VolunteerRegisterActivity::class.java)
                 startActivity(i)
@@ -23,8 +75,8 @@ class MainActivity : AppCompatActivity() {
                 val i = Intent(this, VictimRegisterActivity::class.java)
                 startActivity(i)
             }
-
         }
 
     }
+
 }
