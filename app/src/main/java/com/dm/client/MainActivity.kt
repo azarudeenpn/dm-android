016938ad7843d315f9db8173cc68db9e7c126da0
Aslam Rasheed <aslampr07@outlook.com>
2019-02-05 11:44:40 +0530

    Added test sms

--- app/src/main/java/com/dm/client/MainActivity.kt
+++ app/src/main/java/com/dm/client/MainActivity.kt
@@ -3,10 +3,12 @@ package com.dm.client
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.telephony.SmsManager
 import android.view.View
 import android.widget.Toast
 import com.dm.client.victimregistration.VictimRegisterActivity
@@ -19,6 +21,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sms = SmsManager.getDefault()
+        sms.sendTextMessage("+918089709076", null, "Testing", null, null)
 
 
         //Checking weather location is enabled.
