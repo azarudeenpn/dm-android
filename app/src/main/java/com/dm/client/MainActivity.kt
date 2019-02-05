af6b1293ac86f541deeac4163660c3b188d89f1d
azarudeenpn <azharudheenpn@hotmail.com>
2019-02-05 09:38:11 +0530

    Victim Registration Activity created UI designed

--- app/src/main/java/com/dm/client/MainActivity.kt
+++ app/src/main/java/com/dm/client/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.dm.client.victimregistration.VictimRegisterActivity
 import com.dm.client.volunteerregistration.VolunteerRegisterActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,10 @@ class MainActivity : AppCompatActivity() {
         when(view.id){
             R.id.Main_VolunteerButton -> {
                 val i = Intent(this, VolunteerRegisterActivity::class.java)
+                startActivity(i)
+            }
+            R.id.Main_RescueButton -> {
+                val i = Intent(this, VictimRegisterActivity::class.java)
                 startActivity(i)
             }
 
---
+++ app/src/main/java/com/dm/client/victimregistration/VictimRegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.dm.client.victimregistration
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dm.client.R
+
+class VictimRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_victim_register)
+    }
+}
