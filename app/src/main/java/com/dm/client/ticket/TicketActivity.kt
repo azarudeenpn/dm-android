b800e8023a42fdfbe616ba12d4025d08e177306a
Aslam Rasheed <aslampr07@outlook.com>
2019-02-08 14:46:48 +0530

    Added activity to display ticket for Volunteer

---
+++ app/src/main/java/com/dm/client/ticket/TicketActivity.kt
@@ -0,0 +1,13 @@
+package com.dm.client.ticket
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dm.client.R
+
+class TicketActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket)
+    }
+}
