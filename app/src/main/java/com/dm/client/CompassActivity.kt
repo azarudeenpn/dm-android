cec918fafae733a08d0f5e00d01ee5ce260de250
Azarudeen Pn <azharudheenpn@hotmail.com>
2019-05-14 23:21:14 +0530

    fixed bug compass activity buttons

--- app/src/main/java/com/dm/client/CompassActivity.kt
+++ app/src/main/java/com/dm/client/CompassActivity.kt
@@ -1,14 +1,20 @@
 package com.dm.client
 
 import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.dm.client.ticket.TicketActivity
+import com.dm.client.volunteer.VolunteerActivity
+import java.util.*
 
 class CompassActivity : AppCompatActivity(), SensorEventListener {
 
@@ -16,6 +22,10 @@ class CompassActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
 
     private var sensor: Sensor? = null
+
+    private var vicPhone = ""
+    private var lat = 10f
+    private var lon = 76f
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +48,29 @@ class CompassActivity : AppCompatActivity(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent?) {
         Log.v("dm", event!!.values[0].toString())
         compassView.rotation = 360 - event.values[0]
+    }
+
+    fun buttonClick(view:View){
+        when(view.id){
+            R.id.EndTask_Button -> {
+                val i = Intent(this, VolunteerActivity::class.java)
+                startActivity(i)
+            }
+            R.id.Compass_PhoneButton -> {
+                val i = Intent(Intent.ACTION_DIAL)
+                this.vicPhone = intent.getStringExtra("phone")
+                i.data = Uri.parse("tel:$vicPhone")
+                startActivity(i)
+            }
+            R.id.Compass_MapButton -> {
+                this.lat = intent.getFloatExtra("lat",10f)
+                this.lon= intent.getFloatExtra("lon",76f)
+                val uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?q=loc:%f,%f", lat, lon)
+                val i = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                startActivity(i)
+            }
+
+        }
     }
 
 }
--- app/src/main/java/com/dm/client/services/EndPoints.kt
+++ app/src/main/java/com/dm/client/services/EndPoints.kt
@@ -1,6 +1,6 @@
 package com.dm.client.services
 
 class EndPoints {
-    val hostname = "192.168.0.110:8000"
+    val hostname = "192.168.0.10:8000"
     //val hostname = "139.59.43.100:8383"
 }
