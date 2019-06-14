ce6fb0bd042625784417c7e12f854bb61f109ff7
Aslam Rasheed <aslampr07@outlook.com>
2019-06-14 06:19:35 +0530

    The recieved data is parsed and saved in the database

--- app/src/main/java/com/dm/client/services/CompassListener.kt
+++ app/src/main/java/com/dm/client/services/CompassListener.kt
@@ -1,11 +1,17 @@
 package com.dm.client.services
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.telephony.SmsMessage
 import android.util.Log
 import android.widget.Toast
+import com.dm.client.database.Request
+import com.dm.client.database.RequestDatabase
+import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 
 class CompassListener: BroadcastReceiver() {
@@ -22,12 +28,39 @@ class CompassListener: BroadcastReceiver() {
                 val sms = SmsMessage.createFromPdu(pdus[i] as ByteArray , SmsMessage.FORMAT_3GPP)
 
                 msg += sms.messageBody
-                Log.v("test", "ka")
             }
 
-            val json = JSONObject(msg)
+            Log.v("test", msg)
+            try {
+                val json = JSONArray(msg)
+                val db = RequestDatabase.getInstance(context!!)
 
-            
+                val task = @SuppressLint("StaticFieldLeak")
+                object : AsyncTask<Request, Int, Int>() {
+
+                    override fun doInBackground(vararg params: Request?): Int {
+                        for(i in 0 until json.length()){
+                            val jsonObject = json.getJSONObject(i)
+                            val item = Request(jsonObject["phone"] as String,
+                                jsonObject["name"] as String,
+                                jsonObject["place"] as String,
+                                (jsonObject["latitude"] as Double).toFloat(),
+                                (jsonObject["longitude"] as Double).toFloat(),
+                                jsonObject["creationTime"] as String
+                            )
+                            db?.requestDao()?.insert(item)
+                        }
+                        return 1
+                    }
+
+                }
+                task.execute(null)
+
+
+            }
+            catch (e: JSONException){
+            }
+
         }
     }
 
